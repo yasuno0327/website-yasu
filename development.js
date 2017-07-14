9f11355c5664b30cd453a89460f517af6913d5db
Yoshitomo Yasuno <yasunodayo@yahoo.com>
2017-07-15 00:53:35 +0900

    setting webpack

---
+++ development.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack')
+const path = require('path')
+const dist = path.join(__dirname, 'dist')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+export default {
+  entry: {
+    bundle: __dirname + '/index.js',
+  },
+  output: {
+    path: dist,
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: {
+              module: true,
+              importLoaders: 1,
+              sourceMap: true,
+            }
+          }
+        })
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: dist,
+    port: 8080,
+    inline: true,
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+}
